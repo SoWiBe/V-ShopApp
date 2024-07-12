f3b5d524bd4c06f34a736e30b97c47cc457b675c
Evewibe <aleksey.dantist@gmail.com>
2024-07-12 22:37:35 +0600

    feat: v-shop

---
+++ VShop/Common/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Common;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public string ImageUrl { get; set; }
+}
