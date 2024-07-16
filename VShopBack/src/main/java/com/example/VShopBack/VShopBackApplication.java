f039ab9a660a691a28dca4c15a8bd11804d85935
Evewibe <aleksey.dantist@gmail.com>
2024-07-17 00:23:53 +0600

    feat: backend

---
+++ VShopBack/src/main/java/com/example/VShopBack/VShopBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.VShopBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VShopBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VShopBackApplication.class, args);
+	}
+
+}
