cb8049d627b132ac4601a2ceee438f1e6de35b0b
Evewibe <aleksey.dantist@gmail.com>
2024-07-18 00:22:26 +0600

    feat: remove back and added demo

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.VShopBack;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class VShopBackApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
