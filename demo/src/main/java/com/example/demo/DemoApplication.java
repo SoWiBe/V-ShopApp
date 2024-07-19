aee70e0555e1e25c43020c9b62ccb0cd20272b83
Evewibe <aleksey.dantist@gmail.com>
2024-07-19 16:21:17 +0600

    feat: test endpoints

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,18 @@
 package com.example.demo;
 
+import com.example.demo.product.Product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -17,6 +25,28 @@ public class DemoApplication {
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name){
 		return String.format("Hello %s!, ", name);
+	}
 
+	@GetMapping("/fileinfo")
+	public String fileInfo(){
+		String file = "C:\\Users\\aleks\\OneDrive\\Рабочий стол\\javafile.txt";
+		StringBuilder stringBuilder = new StringBuilder();
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(file));
+			for (String s : lines){
+				stringBuilder.append(s);
+			}
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+		return stringBuilder.toString();
+	}
+
+	@GetMapping("/products")
+	public List<Product> products(){
+		return List.of(
+				new Product(1L, "Test", 350)
+		);
 	}
 }
---
+++ demo/src/main/java/com/example/demo/product/Product.java
@@ -0,0 +1,55 @@
+package com.example.demo.product;
+
+public class Product {
+    private Long id;
+    private String title;
+    private double price;
+
+    public Product(){
+
+    }
+
+    public Product(Long id, String title, double price) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+    }
+
+    public Product(String title, double price) {
+        this.title = title;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
