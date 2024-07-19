1289168e019e758c1b1d7ba52b7051323e52d876
Evewibe <aleksey.dantist@gmail.com>
2024-07-19 16:47:32 +0600

    fix: demo application adn products service

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -42,6 +42,4 @@ public class DemoApplication {
 
 		return stringBuilder.toString();
 	}
-
-
 }
--- demo/src/main/java/com/example/demo/product/ProductsService.java
+++ demo/src/main/java/com/example/demo/product/ProductsService.java
@@ -1,6 +1,5 @@
 package com.example.demo.product;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
