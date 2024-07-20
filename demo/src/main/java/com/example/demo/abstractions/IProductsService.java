94f141432178347e96a497b112b32930f7950504
Evewibe <aleksey.dantist@gmail.com>
2024-07-20 09:57:59 +0600

    feat: abstractions

---
+++ demo/src/main/java/com/example/demo/abstractions/IProductsService.java
@@ -0,0 +1,9 @@
+package com.example.demo.abstractions;
+
+import com.example.demo.product.Product;
+
+import java.util.List;
+
+public interface IProductsService {
+    List<Product> getProducts();
+}
--- demo/src/main/java/com/example/demo/product/ProductsService.java
+++ demo/src/main/java/com/example/demo/product/ProductsService.java
@@ -1,11 +1,12 @@
 package com.example.demo.product;
 
+import com.example.demo.abstractions.IProductsService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class ProductsService {
+public class ProductsService implements IProductsService {
 
     public List<Product> getProducts(){
         return List.of(
