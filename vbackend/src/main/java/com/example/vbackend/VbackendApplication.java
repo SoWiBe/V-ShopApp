dbebfab3c410eed75bf06f6247cde22719832213
Evewibe <aleksey.dantist@gmail.com>
2024-07-21 12:56:57 +0600

    feat: not working bean injection

--- vbackend/src/main/java/com/example/vbackend/VbackendApplication.java
+++ vbackend/src/main/java/com/example/vbackend/VbackendApplication.java
@@ -2,8 +2,12 @@ package com.example.vbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@ComponentScan(basePackages = "com.example.vbackend")
 public class VbackendApplication {
 
 	public static void main(String[] args) {
