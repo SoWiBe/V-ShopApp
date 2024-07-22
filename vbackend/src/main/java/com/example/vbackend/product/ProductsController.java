48109160a235440166dbda70e475c8d5932f0069
Evewibe <aleksey.dantist@gmail.com>
2024-07-22 06:18:18 +0600

    feat: uncomment any crud endpoints

--- vbackend/src/main/java/com/example/vbackend/product/ProductsController.java
+++ vbackend/src/main/java/com/example/vbackend/product/ProductsController.java
@@ -6,6 +6,8 @@ import com.example.vbackend.infrastructure.service.ProductsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(path = "api/v1/product")
 public class ProductsController {
@@ -18,19 +20,24 @@ public class ProductsController {
         return productsService.createProduct(product);
     }
 
-//    @GetMapping("/{id}")
-//    public Product getProductById(@PathVariable String id){
-//        return productsService.getProductById(id).orElse(null);
-//    }
-//
-//    @PutMapping("/{id}")
-//    public Product updateProduct(@PathVariable String id, @RequestBody Product product){
-//        product.setId(id);
-//        return productsService.updateProduct(product);
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public void deleteProduct(@PathVariable String id){
-//        productsService.deleteProduct(id);
-//    }
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable String id){
+        return productsService.getProductById(id).orElse(null);
+    }
+
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable String id, @RequestBody Product product){
+        product.setId(id);
+        return productsService.updateProduct(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable String id){
+        productsService.deleteProduct(id);
+    }
+
+    @GetMapping
+    public List<Product> getProducts(){
+        return productsService.getProducts();
+    }
 }
