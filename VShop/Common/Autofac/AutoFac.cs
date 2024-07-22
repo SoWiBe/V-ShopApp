ff0b9518904f6ef154b8f7f81cde9f3a5dacf00d
Evewibe <aleksey.dantist@gmail.com>
2024-07-22 07:13:37 +0600

    feat: AutoFac.cs

--- VShop/Common/Autofac/AutoFac.cs
+++ VShop/Common/Autofac/AutoFac.cs
@@ -1,6 +1,20 @@
-﻿namespace Common.Autofac;
+﻿using Autofac;
 
-public class AutoFac
+namespace Common.Autofac;
+
+public sealed class AutoFac
 {
+    private AutoFac()
+    {
     
+    }
+
+    public static AutoFac Default { get; } = new();
+    public ContainerBuilder Builder { get; }
+    public IContainer Container { get; private set; }
+
+    public void Build()
+    {
+        Container = Builder.Build();
+    }
 }
